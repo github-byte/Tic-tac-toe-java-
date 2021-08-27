2d561eb4487b420bff310a7d9061a95eb0b573be
Dipannita <dipannita223@gmail.com>
2021-08-27 18:46:30 +0530

    Second commit

--- ticTacToe.java
+++ ticTacToe.java
@@ -52,7 +52,7 @@ public class ticTacToe {
             System.out.println();
         }
     
-            if(isWinner(board,n1,p1)){
+            if(isWinner(board,n1)){
                  System.out.println("Player 1 wins");
                 break;
             }
@@ -77,7 +77,7 @@ public class ticTacToe {
             }
             System.out.println();
         }
-            if(isWinner(board,n2,p2)){
+            if(isWinner(board,n2)){
                 System.out.println("Player 2 wins");
                 break;
             }
@@ -113,7 +113,7 @@ public class ticTacToe {
            int x=Integer.parseInt(s[0]);
            int y=Integer.parseInt(s[1]); 
             
-            if(x<=board.length-1 && y<=board[0].length-1){
+            if(x<3 && y<3){
                 if(board[x][y]=='-'){
                     board[x][y]=p;
                     return true;
@@ -129,147 +129,27 @@ public class ticTacToe {
         
         //check winner
         
-        public static boolean isWinner(char[][] board,String given,char alpha){
+        public static boolean isWinner(char[][] board,String given){
            String[] s=given.split(",");
            int x=Integer.parseInt(s[0]);
            int y=Integer.parseInt(s[1]);
             //check rows
-            char checkRow=board[0][y];
-            boolean rowFlag=true;
             
-            for(int i=1;i<board.length;i++){
-                if(checkRow!='-'){
-                if(board[i][y]!='-'){
-                    if(board[i][y]!=checkRow){
-                        rowFlag=false;
-                    }
-                  
-                }
-                if(board[i][y]=='-'){
-                    rowFlag=false;
-                }
-                   checkRow=board[i][y];
-                }
-                
-                if(checkRow=='-'){
-                    rowFlag=false;
-                }        
-                
-                
-            }
-            
-    
-            
-            
-            //check colums
-    
-            char checkCol=board[x][0];
-            
-          
-            boolean colFlag=true;
            
-            for(int j=1;j<board[0].length;j++){
-                if(checkCol!='-'){
-                if(board[x][j]!='-'){
-                      if(board[x][j]!=checkCol){
-                         colFlag=false;
-                         
+               if( board[x][0]!='-' && board[x][0]==board[x][1] && board[x][1]==board[x][2]){
+                    return true;
+            }
+            else if(board[0][y]!='-'  && board[0][y]==board[1][y] && board[1][y]==board[2][y]){
+                    return true;
+            }
+            else if(x+y==board.length-1 || x==y){
+                    if(board[0][0]!='-' && board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+                        return true;
                     }
-                   
-                         checkCol=board[x][j];
-                }
-                if(board[x][j]=='-'){
-                    colFlag=false;
-                }
-                    
-                }
-                if(checkCol=='-'){
-                    colFlag=false; 
-                }
-            }
-            
-            if(colFlag==true || rowFlag==true){
-                return true;
-            }
-            
-            //check diag
-            
-          if(x==y){
-            char diag1=board[0][0];
-            boolean checkDiag1=true;
-   
-            
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board[0].length;j++){
-                  
-                    if(i==j){
-                //check if board[i][j] and checkdiag!=-
-                //if not check values
-                if(board[i][j]=='-' || diag1=='-'){
-                    checkDiag1=false;
-                }
-                else if(board[i][j]!='-' && diag1!='-'){
-                        if(board[i][j]!=diag1){
-                            checkDiag1=false;
-                            diag1=board[i][j];
-                        }
-                }
-                    }
-                    
-                
-         
-    
-            }
-
-            }
-
-            if(checkDiag1==true){
-               return true;
-           }
-
-            
-          }
-            
-          if(x+y==board.length-1){
-            char diag2=board[0][2];
-            boolean checkDiag2=true;
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board[0].length;j++){
-                  
-                   if(i+j==board.length-1){
-                      if(i+j==board.length-1){
-                 
-            if(board[i][j]=='-' || diag2=='-'){
-                checkDiag2=false;
-            }
-            else if(board[i][j]!='-' && diag2!='-'){
-                    if(board[i][j]!=diag2){
-                        checkDiag2=false;
-                        diag2=board[i][j];
+                    if(board[0][2]!='-' && board[0][2]==board[1][1] && board[1][1]==board[2][1]){
+                        return true;
                     }
             }
-
-    }
-                   }
-                
-         
-    
-            }
-
-            }
-
-            
-            if(checkDiag2==true){
-                return true;
-            }
- 
-    
-          }
-            
-    
-
-
-
 
             return false;
             
